ffa0bc97d6a547947ccaaaf692dffa2c30f0914d
Vladislav <v.kirbaba@gmail.com>
2015-11-24 16:34:47 +0300

    Header

---
+++ index.php
@@ -0,0 +1,124 @@
+﻿<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE">
+    </script>
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>	
+	<div class="topline-soc">
+		<div class="container">
+			<div class="row">
+				<div class="skew-wrap">
+					<div class="topline-soc--logobord"></div>
+					<div class="topline-soc--but">
+						<a href="#"><i class="fa fa-vk"></i></a>
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-odnoklassniki"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<header class="header">
+		<div class="container">	
+			<div class="row">
+				<div class="skew-wrap">		
+					<div class="header__navigation">
+						<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 p0">
+							<a href="#" class="header__navigation--logo">
+								<span><b>R</b></span>
+								<h3>RUSMUSIC</h3>
+							</a>
+						</div>
+						<div class="col-lg-9 col-md-9 col-sm-6 col-xs-12 p0">
+							<div class="header__navigation--menu">								
+								<div class="navbar-header">
+									<a href="#nowhere" class="hamburger-slim" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"></a>
+									<div class="navbar-brand">
+										
+									</div>     		      
+								</div>
+								
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">		      	     
+									<ul class="nav navbar-nav navbar-center navigation__list">		      
+										<li><a href="#">О компании</a></li>
+										<li class="dropdown"><a href="#" data-toggle="dropdown">НАПРАВЛЕНИЯ</a>
+											<ul class="dropdown-menu">
+												<li>
+													<a href='#'>Детская творческая мастерская</a>
+												</li>
+												<li>
+													<a href='#'>Организация концертов фестивалей, Дни Рождения</a>
+												</li>
+												<li>
+													<a href='#'>Стань звездой</a>
+												</li>
+												<li>
+													<a href='#'>Обучение</a>
+												</li>
+												<li>
+													<a href='#'>Заработай на музыке</a>
+												</li>
+												<li>
+													<a href='#'>Запиши хит</a>
+												</li>
+												<li>
+													<a href='#'>Найди свой хит</a>
+												</li>
+											</ul>
+										</li>
+										<li><a href="#">Новости</a></li>
+										<li><a href="#">Контакты</a></li>	        		       
+									</ul>
+									<div class="header__account">
+										<a href="#" class="header__navigation--menu--account"><span>регистрация</span></a>
+										<a href="#" class="header__navigation--menu--account"><span>войти</span></a>
+									</div>
+									<div class="header__search">
+										<input type="text" name="search--input" class="header__search--input">
+										<a href="#" class="header__search--butt"><i class="fa fa-search"></i></a>
+									</div>	
+
+								</div><!-- /.navbar-collapse -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
+		</div>	
+	</header>
+
+
+	<section class="enter-slideshow">		
+		<div class="fotorama" data-width="100%"  data-ratio="1568/658" data-fit="cover">			
+			<div data-img="<?php bloginfo('template_directory'); ?>/img/slide1.jpg">
+				<div class="container">
+					<div class="enter-slideshow__text">					
+						<h1>Rihanna</h1>
+						<h4>НОВЫЙ КОНЦЕРТНЫЙ ТУР ПО РОССИИ И КАЗАХСТАНУ</h4>
+					</div>		
+				</div>		
+			</div>
+			<div data-img="<?php bloginfo('template_directory'); ?>/img/slide1.jpg">
+				<div class="container">
+					<div class="enter-slideshow__text">					
+						<h1>Slide #2</h1>
+						<h4>НОВЫЙ КОНЦЕРТНЫЙ ТУР ПО РОССИИ И КАЗАХСТАНУ</h4>
+					</div>
+				</div>
+			</div>			
+		</div>	
+	</section>
+
+    
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,92 @@
+
+/*-------------GOOGLE MAPS-----------------*/
+
+/*function initialize() {
+
+    var myLatlng = new google.maps.LatLng(59.934602, 30.334607);
+    var mapOptions = {
+        center: new google.maps.LatLng(59.934602, 30.334607),
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title:"Ditlogistic"
+    });
+}
+
+function loadScript() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE&callback=initialize";
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+*/
+
+
+jQuery(function($) {
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({scrollTop: 0}, 1000);
+    });
+
+    $('.smoothScroll').click(function(event) {
+        event.preventDefault();
+        var href=$(this).attr('href');
+        var target=$(href);
+        var top=target.offset().top;
+        $('html,body').animate({
+            scrollTop: top
+        }, 1000);
+    });
+});
+
+//jQuery( '.header__navigation--menu li:has(ul)' ).doubleTapToGo();
+jQuery(function ($) {
+    $('.hamburger-slim').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('hamburger-slim--active');
+    });
+   
+    $('.header__search--butt').on('click', function (e) {        
+        if ( $('.header__search--input').hasClass('header__search--input--active')){
+             $('.header__search--input').animate({
+                width: '0'
+            }, 300, function(){
+                $('.header__search--input').css({
+                    display: 'none',
+                    'border-width': '0'
+                });                 
+            });
+             $('.header__search--butt').animate({
+                'margin-left': '50px'
+            });                      
+        }
+        else {
+            $('.header__search--input').css({
+                display: 'block',
+                'border-width': '1'
+            });             
+            $('.header__search--input').animate({
+                width: '130px'
+            }, 300);
+            $('.header__search--butt').animate({
+                'margin-left': '0'
+            });
+        };
+        $('.header__search--input').toggleClass('header__search--input--active');
+    });
+});
