2cd16bbc017dac6d46aadb47e94f471396858b33
Vladislav <v.kirbaba@gmail.com>
2015-12-01 14:32:44 +0300

    Команда. Адаптив

--- single.php
+++ single.php
@@ -15,7 +15,7 @@
 			</div>
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<?php if ( has_post_thumbnail() ): ?>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+			<div class="col-lg-4 col-md-4 col-sm-5 col-xs-12">
 				<div class="team-carousel__item">
 					<div class="team-carousel__item--img">
 						<?php the_post_thumbnail('full', array()); ?>							
@@ -35,7 +35,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+			<div class="col-lg-8 col-md-8 col-sm-7 col-xs-12">
 				<article class="team__bio">
 					<?php the_content(); ?>
 				</article>
