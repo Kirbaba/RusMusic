9f8dc7e75570fe51948a191e9c0bcaf9efce697f
Vladislav <v.kirbaba@gmail.com>
2015-12-02 12:51:58 +0300

    Категория - новости - адаптив

--- category-news.php
+++ category-news.php
@@ -100,8 +100,7 @@
 				<li class="active"><a href="#">1</a></li>
 				<li><a href="#">2</a></li>
 				<li><a href="#">3</a></li>
-				<li><a href="#">4</a></li>
-				<li><a href="#">5</a></li>
+				<li><a href="#">4</a></li>				
 				<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/RIGHT-ARROW.png" alt=""></a></li>
 			</ul>
 		</div>
