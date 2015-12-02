9ebff51a3b1f7465770141862f444f29b95417ec
Vladislav <v.kirbaba@gmail.com>
2015-12-02 12:43:15 +0300

    Категория новости

---
+++ category-news.php
@@ -0,0 +1,143 @@
+<? get_header() ?>
+
+	<section class="news-page">
+		<div class="container">
+			<div class="row">
+				<header class="news-page--head">						
+					<h3><span>Новости</span></h3>
+					<div class="news-page--social">
+						<a href="#"><i class="fa fa-vk"></i></a>
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-odnoklassniki"></i></a>
+					</div>
+				</header>
+			</div>
+			<div class="row">
+				<article class="news-page__item--first">
+					<div class="news-page__item--first--img">
+						<img src="<?php bloginfo('template_directory'); ?>/img/kanye_west.jpg" alt="">
+					</div>
+					<div class="col-lg-8 col-md-8 col-sm-10 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-1">
+						<small class="news__author">Written by <a href="#">Eric Snorker</a> on <span>Sunday 25 December 2016.</span></small>
+						<h3>MARK YOUR CALENDARS — THE 2016 VMAS WILL AIR ON …</h3>
+						<p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, 
+							lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+							Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Sed ut imperdiet nisi. 
+							Proin condimentum fermentum nunc. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. 
+							Sed ut imperdiet nisi. Proin condimentum fermentum nunc.</p>
+						<a href="#" class="news--read">Читать</a>
+					</div>
+				</article>
+			</div>
+			<div class="row">
+				<article class="news-page__item">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+						<div class="news-page__item--img">
+							<a href="#">
+								<img src="<?php bloginfo('template_directory'); ?>/img/miley_cyrus.jpg" alt="">
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+						<div class="news-page__item--description">
+							<h3>MILEY CYRUS & THE FLAMING LIPS "DOOO IT (LIVE & UNCENSORED)"</h3>
+							<small class="news__author">Written by <a href="#">Eric Snorker</a> on <span>Sunday 25 December 2016.</span></small>
+							<p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, 
+								lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+								Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.  
+								Aenean sollicitudin, lorem quis bibendum sed odio sit.</p>
+								<a href="#" class="news--read">Читать</a>
+						</div>
+					</div>
+				</article>
+			</div>
+			<div class="row">
+				<article class="news-page__item">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+						<div class="news-page__item--img">
+							<a href="#">
+								<img src="<?php bloginfo('template_directory'); ?>/img/miley_cyrus.jpg" alt="">
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+						<div class="news-page__item--description">
+							<h3>MILEY CYRUS & THE FLAMING LIPS "DOOO IT (LIVE & UNCENSORED)"</h3>
+							<small class="news__author">Written by <a href="#">Eric Snorker</a> on <span>Sunday 25 December 2016.</span></small>
+							<p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, 
+								lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+								Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.  
+								Aenean sollicitudin, lorem quis bibendum sed odio sit.</p>
+								<a href="#" class="news--read">Читать</a>
+						</div>
+					</div>
+				</article>
+			</div>
+			<div class="row">
+				<article class="news-page__item">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+						<div class="news-page__item--img">
+							<a href="#">
+								<img src="<?php bloginfo('template_directory'); ?>/img/miley_cyrus.jpg" alt="">
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+						<div class="news-page__item--description">
+							<h3>MILEY CYRUS & THE FLAMING LIPS "DOOO IT (LIVE & UNCENSORED)"</h3>
+							<small class="news__author">Written by <a href="#">Eric Snorker</a> on <span>Sunday 25 December 2016.</span></small>
+							<p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, 
+								lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+								Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.  
+								Aenean sollicitudin, lorem quis bibendum sed odio sit.</p>
+								<a href="#" class="news--read">Читать</a>
+						</div>
+					</div>
+				</article>
+			</div>
+			<ul class="pagination">
+				<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/LEFT-ARROW.png" alt=""></a></li>
+				<li class="active"><a href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">5</a></li>
+				<li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/RIGHT-ARROW.png" alt=""></a></li>
+			</ul>
+		</div>
+	</section>
+	
+	<section class="partners">
+		<div class="container">
+			<div class="row">				
+				<h4>НАШИ ПАРТНЕРЫ</h4>
+				
+				<div class="col-lg-2 col-lg-offset-1 col-md-2 col-md-offset-1 col-sm-4 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_03.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_05.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_07.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-sm-offset-2 col-lg-offset-0 col-md-offset-0 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_09.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 col-xs-offset-3 col-sm-offset-0">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_11.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<? get_footer() ?>
