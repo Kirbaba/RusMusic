403d767b5d73382e68ba1a48b2c4e550933be1f3
Vladislav <v.kirbaba@gmail.com>
2015-12-03 12:04:56 +0300

    Организация концертов - услуги

---
+++ page-concert_organizing.php
@@ -0,0 +1,86 @@
+<? get_header()?>
+	<section class="page-title-head organizing__head">
+		<div class="container-fluid">
+			<div class="row">
+				<h1>Организация <br>концертов</h1>
+			</div>
+		</div>		
+	</section>
+
+	<section class="organizing__services">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 col-sm-6 col-xs-12">
+					<h2>Мы занимаемся организацией концертов</h2>
+					<h4>Proin gravida nibh vel velit auctor aliqueat. </h4>
+					<p>Proin gravida nibh vel velit auctor aliquet. 
+						Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum,
+						 nec sagittis sem nibh id elit. 
+						 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. 
+						 Sed ut imperdiet nisi. Proin condimentum fermentum nunc. 
+						 Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. 
+						 Sed ut imperdiet nisi. Proin condimentum fermentum nunc.</p>
+				</div>
+				<div class="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-6 col-xs-12 p0">
+					<a href="#" class="organizing__services__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/slide1.jpg" alt="">
+						<span class="organizing__services__item--open"><i class="fa fa-search"></i></span>
+						<span class="organizing__services__item--name">Услуга номер 1</span>
+						<span class="organizing__services__item--shadow"></span>
+					</a>
+					<a href="#" class="organizing__services__item">
+						<span class="organizing__services__item--open"><i class="fa fa-search"></i></span>
+						<span class="organizing__services__item--name">Услуга номер 1</span>
+						<span class="organizing__services__item--shadow"></span>
+					</a>
+					<a href="#" class="organizing__services__item">
+						<span class="organizing__services__item--open"><i class="fa fa-search"></i></span>
+						<span class="organizing__services__item--name">Услуга номер 1</span>
+						<span class="organizing__services__item--shadow"></span>
+					</a>
+					<a href="#" class="organizing__services__item">
+						<span class="organizing__services__item--open"><i class="fa fa-search"></i></span>
+						<span class="organizing__services__item--name">Услуга номер 1</span>
+						<span class="organizing__services__item--shadow"></span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+	<section class="partners">
+		<div class="container">
+			<div class="row">				
+				<h4>НАШИ ПАРТНЕРЫ</h4>
+				
+				<div class="col-lg-2 col-lg-offset-1 col-md-2 col-md-offset-1 col-sm-4 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_03.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_05.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_07.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-sm-offset-2 col-lg-offset-0 col-md-offset-0 col-xs-6">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_09.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6 col-xs-offset-3 col-sm-offset-0">
+					<div class="partners__item">
+						<img src="<?php bloginfo('template_directory'); ?>/img/partners_11.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<? get_footer()?>
