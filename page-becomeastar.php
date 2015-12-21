44745b578b1a396ae911559b66690d4f37324f03
Vladislav <v.kirbaba@gmail.com>
2015-12-21 15:02:37 +0300

    Стань звездой

---
+++ page-becomeastar.php
@@ -0,0 +1,80 @@
+<? get_header()?>
+	<section class="page-title-head becomeastar__head">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="becomeastar__triangle"></div>
+				<header class="becomeastar__head--text">						
+					<h2>Стань звездой</h2>
+					<p>Оказываем полный спектр услуг классического PR-агентства:<br> 
+	строим отношения с журналистами, продвигаем в социальных сетях</p>
+				</header>
+			</div>
+		</div>		
+	</section>
+
+	<section class="becomeastar__content">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5 col-md-5 col-sm-4 col-xs-12">
+					<aside class="becomeastar__menu">
+						<ul>
+							<li><a href="#">Сценический образ</a></li>
+							<li><a href="#">Музыкальное направление</a></li>
+							<li><a href="#">Имидж</a></li>
+							<li><a href="#">Образ для СМИ</a></li>
+							<li><a href="#">Имиджевые фотосессии <br>Помощь в подборе материала</a></li>
+							<li><a href="#">Написание аранжировок</a></li>
+							<li><a href="#">Написание и корректировка
+текстов песен</a></li>
+							<li><a href="#">Запись демо</a></li>
+							<li><a href="#">Запись и выпуск альбомов</a></li>
+							<li><a href="#">Работа с радиостанциями
+Заключение контрактов на трек
+с музыкальными издательствами
+</a></li>
+							<li><a href="#">Помощь в подписании контрактов
+с цифровыми дистрибьютерскими
+компаниями</a></li>
+						</ul>
+					</aside>
+				</div>
+				<div class="col-lg-7 col-md-7 col-sm-8 col-xs-12">
+					<article class="becomeastar__textbox">
+						<p>			Этот дивный новый мир… В нем не покупают музыку альбомами и не удивляются миллиарду просмотров на Youtube. Лучшим способом заявить о себе стал Instagram, а закрытое платье превратилось в такую редкость, что может вызвать скандал. В новом мире не работают старые схемы продвижения. Сегодня они уже банальны, а завтра будут вредны. Когда невозможно опереться на опыт, приходит время изобретателей.
+
+RusMusic – это продюсерский центр нового поколения, не имеющий аналогов в России. Команда центра создает и внедряет новые маркетинговые инструменты и ищет инновационные подходы в области public relations. То, что pr-агентства будут использовать через несколько лет, Starfactory внедряет уже сегодня.
+
+Мы изобретаем и развиваем новые образы для Celebrity, индивидуальные стратегии продвижения, открываем New Faces, ускоряем динамику развития, модернизируем PR стратегию для Celebrity, в  том числе и для испытывающих синдром “творческого кризиса”.
+
+В нашей работе мы используем все известные методы продвижения, применяемые в мире: social media marketing, телевизионные проекты, fashion events, работа с репутацией и многие другие. Реализация индивидуальных стратегий продвижения осуществляется согласно высоким стандартам качества. 
+
+Но основной акцент мы делаем на более прогрессивные алгоритмы и эффективные методы, которые, вероятно, войдут в общепринятую практику только через несколько лет.</p>
+					</article>
+				</div>
+				
+				
+			</div>
+		</div>
+	</section>
+
+	<section class="becomeastar__questionnaire">
+		<div class="container">
+			<div class="row">
+				<h3>Заполнить анкету о себе</h3>
+				<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12 col-xs-12 p0">					
+					<input type="text" class="registration-input--large" name="becomeastar-name" placeholder="Ваше имя">
+					<input type="text" class="registration-input--large" name="becomeastar-email" placeholder="Адрес электронной почты:">
+					<input type="password" class="registration-input--large" name="becomeastar" placeholder="Город">
+					<input type="password" class="registration-input--large" name="becomeastar-facebook" placeholder="Facebook">
+					<input type="password" class="registration-input--large" name="becomeastar-instagram" placeholder="Instagram">
+					<input type="password" class="registration-input--large" name="becomeastar-site" placeholder="Сайт">
+					<input type="password" class="registration-input--large" name="becomeastar-songs-place" placeholder="Где можно послушать Ваши песни">
+					<div class="becomeastar__questionnaire--but">
+  						<input type="button" name="becomeastar-input--button" class="registration-input--button registerUser" value="Отправить">
+  					</div>
+				</div>
+				
+			</div>
+		</div>
+	</section>
+<? get_footer()?>
