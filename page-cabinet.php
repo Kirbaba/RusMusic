90795706306d8e9d8bb5bec1dd3194c24ce50290
Vladislav <v.kirbaba@gmail.com>
2016-01-26 16:31:16 +0300

    ЛК - Доска

--- page-cabinet.php
+++ page-cabinet.php
@@ -37,7 +37,7 @@
 							<a href="#"><i class="fa fa-commenting-o"></i>Сообщения<span class="counter--active">32</span></a>
 						</li>
 						<li>
-							<a href="#"><i class="fa fa-flask"></i>Проекты<span class="counter">12</span></a>
+							<a href="#nowhere"><i class="fa fa-flask"></i>Проекты<span class="counter">12</span></a>
 							<ul class="cabinet__sidebar--menu--bottom">
 								<li><a href="#">На рассмотрении <span class="counter">1</span></a></li>
 								<li><a href="#">В работе<span class="counter--passive">23</span></a></li>
@@ -49,6 +49,107 @@
 						<a href="#">НАЙДИ СВОЙ ХИТ</a>
 					</div>
 				</aside>
+			</div>
+			<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+				<div class="row">				
+					<div class="col-xs-6">
+						<div class="cabinet__processes mastering">
+							<div class="cabinet__processes--icon"></div>
+							<div class="cabinet__processes--title">
+								<h4>НА МАСТЕРИНГЕ</h4>
+								<h3>24 композиции</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-6">
+						<div class="cabinet__processes reduction">
+							<div class="cabinet__processes--icon"></div>
+							<div class="cabinet__processes--title">
+								<h4>НА СВЕДЕНИИ</h4>
+								<h3>12 композиции</h3>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12">
+						<div class="cabinet__board">
+							<article class="cabinet__board__item dropping">
+								<div class="cabinet__board__item--head">
+									<small>СЕГОДНЯ, 12:30</small>
+								</div>
+								<span class="cabinet__board__item--percentage">23%</span>
+								<div class="cabinet__board__item--title">
+									<h3>«<span>Песня №1</span>» в стадии сведения</h3>
+								</div>
+								<div class="cabinet__board__item--controls">
+									<span class="cabinet__board__item--controls--type">СВЕДЕНИЕ</span>
+									<a href="#" class="cabinet__board__item--controls--dropbtn">
+										<i class="fa fa-angle-down"></i>
+									</a>
+								</div>
+							</article>
+
+							<article class="cabinet__board__item">
+								<div class="cabinet__board__item--head">
+									<small>СЕГОДНЯ, 12:30</small>
+								</div>
+								<div class="cabinet__board__item--title">
+									<h3>Вы были в разделе «О компании»</h3>
+								</div>
+							</article>
+
+							<article class="cabinet__board__item dropping">
+								<div class="cabinet__board__item--head">
+									<small>СЕГОДНЯ, 12:30</small>
+								</div>
+								<span class="cabinet__board__item--percentage">83%</span>
+								<div class="cabinet__board__item--title">
+									<h3>Композиция «<span>лето</span>» в стадии мастеринга</h3>
+								</div>
+								<div class="cabinet__board__item--controls">
+									<span class="cabinet__board__item--controls--type">МАСТЕРИНГ</span>
+									<a href="#" class="cabinet__board__item--controls--dropbtn">
+										<i class="fa fa-angle-down"></i>
+									</a>
+								</div>
+								
+							</article>
+							<div class="cabinet__board__item--open">
+								<article class="cabinet__board__item from-admin">
+									<div class="cabinet__board__item--head">
+										<small>СЕГОДНЯ, 12:30</small>
+										<small>ОТВЕТ ОТ АДМИНИСТРАТОРА</small>
+									</div>
+									<div class="cabinet__board__item--comment ">
+										<p>До мастеринга внимательно прослушайте материал. 
+										Во многих музыкальных жанрах вокал является наиболее важным. 
+										В других стилях это ритм, в некоторых - дистошн и так далее. </p>
+										<a href="#" class="cabinet__board__item--comment--reply">ОТВЕТИТЬ</a>
+									</div>
+								</article>
+								<article class="cabinet__board__item from-admin">
+									<div class="cabinet__board__item--head">
+										<small>СЕГОДНЯ, 12:30</small>
+										<small>ОТВЕТ ОТ АДМИНИСТРАТОРА</small>
+									</div>
+									<div class="cabinet__board__item--comment ">
+										<p>До мастеринга внимательно прослушайте материал. 
+										Во многих музыкальных жанрах вокал является наиболее важным. 
+										В других стилях это ритм, в некоторых - дистошн и так далее.  </p>
+										<textarea name="mes" class="cabinet__board__item--comment--text"></textarea>
+										<a href="#" class="cabinet__board__item--comment--reply">ОТПРАВИТЬ</a>
+									</div>
+								</article>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+				<div class="cabinet__forwarding">
+					<a href="#" class="cabinet__forwarding--mastering">ОТПАРВИТЬ НА МАСТЕРИНГ</a>
+					<a href="#" class="cabinet__forwarding--reduction">ОТПАРВИТЬ НА СВЕДЕНИЕ</a>
+					<a href="#" class="cabinet__forwarding--instructions">Инструкция по подготовке файлов для мастеринга и сведения</a>
+				</div>
 			</div>
 		</div>
 	</div>
